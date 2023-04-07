d6ef16df99d2cab0663460bcfd155ca89b12201c
immprada <im.mprada@gmail.com>
2023-04-07 10:15:24 -0500

    feat(migrations): add user and clubhouse foreign keys to memberships table

--- db/migrate/20230406182937_create_memberships_table.rb
+++ db/migrate/20230406182937_create_memberships_table.rb
@@ -1,6 +1,8 @@
 class CreateMembershipsTable < ActiveRecord::Migration[7.0]
   def change
     create_table :memberships_tables do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :clubhouse, null: false, foreign_key: true
 
       t.timestamps
     end
