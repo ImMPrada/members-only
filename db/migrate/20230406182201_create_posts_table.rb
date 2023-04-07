26d344f9e3372b2a41df591568b1e7e1f01670bc
immprada <im.mprada@gmail.com>
2023-04-07 10:13:04 -0500

    feat(migration): add content, user_id, and clubhouse_id columns to posts table

--- db/migrate/20230406182201_create_posts_table.rb
+++ db/migrate/20230406182201_create_posts_table.rb
@@ -1,6 +1,9 @@
 class CreatePostsTable < ActiveRecord::Migration[7.0]
   def change
     create_table :posts_tables do |t|
+      t.text :content, null: false
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :clubhouse, null: false, foreign_key: true
 
       t.timestamps
     end
