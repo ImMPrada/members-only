cce4441c21483a7a766d0bd1ad1f925c4136b73e
immprada <im.mprada@gmail.com>
2023-04-07 10:11:38 -0500

    feat(migration): add Clubhouse table with title, description, and admin_id columns feat(migration): add unique index on admin_id and title columns in Clubhouse table

--- db/migrate/20230406181952_create_clubhouses_table.rb
+++ db/migrate/20230406181952_create_clubhouses_table.rb
@@ -1,8 +1,13 @@
 class CreateClubhousesTable < ActiveRecord::Migration[7.0]
   def change
     create_table :clubhouses_tables do |t|
+      t.text :title, null: false
+      t.text :description
+      t.belongs_to :admin, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :clubhouse, %i[admin_id title], unique: true
   end
 end
