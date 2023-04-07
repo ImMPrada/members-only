41e0a16dd5de3bec9cc58c90bffd096e8e91b59d
immprada <im.mprada@gmail.com>
2023-04-07 10:14:01 -0500

    feat(db): add content, user_id, and post_id columns to comments table

--- db/migrate/20230406182218_create_comments_table.rb
+++ db/migrate/20230406182218_create_comments_table.rb
@@ -1,6 +1,9 @@
 class CreateCommentsTable < ActiveRecord::Migration[7.0]
   def change
     create_table :comments_tables do |t|
+      t.text :content, null: false
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :post, null: false, foreign_key: true
 
       t.timestamps
     end
