0f9c0cb329000312892a847968a98086b36bd0c0
immprada <im.mprada@gmail.com>
2023-04-06 22:34:03 -0500

    feat(migration): create users table with username and email columns and add unique index to both columns

--- db/migrate/20230406182425_create_users_table.rb
+++ db/migrate/20230406182425_create_users_table.rb
@@ -1,8 +1,13 @@
 class CreateUsersTable < ActiveRecord::Migration[7.0]
   def change
     create_table :users_tables do |t|
+      t.string :username, null: falsen, unique: true
+      t.string :email, null: false, unique: true
 
       t.timestamps
     end
+
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
   end
 end
