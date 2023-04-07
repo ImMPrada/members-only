434ba228e547630b2e21cf784e48044f27bed313
immprada <im.mprada@gmail.com>
2023-04-07 10:14:38 -0500

    fix(db): fix typo in admins table name feat(db): add user foreign key to admins table

--- db/migrate/20230406182620_create_admins_table.rb
+++ db/migrate/20230406182620_create_admins_table.rb
@@ -1,6 +1,7 @@
 class CreateAdminsTable < ActiveRecord::Migration[7.0]
   def change
     create_table :admins_tables do |t|
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
