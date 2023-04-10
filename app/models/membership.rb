57cbd80e71fed373d81ddbef854644e989a86479
immprada <im.mprada@gmail.com>
2023-04-10 12:12:56 -0500

    feat(membership.rb): add validation for role attribute to ensure it is either member, admin, or owner

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :clubhouse
+
+  validates :role, presence: true, inclusion: { in: %w[member admin owner],
+                                                message: "#{value} is not a valid size" }
 end
