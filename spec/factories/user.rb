c0649fb75e8935a80b0e579b9ad39d03dbb05343
immprada <im.mprada@gmail.com>
2023-04-10 11:30:29 -0500

    configuration

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory(:user) do
+  factory :user do
+    username { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
   end
 end
