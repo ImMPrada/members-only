7704d1a14bd2e80b41ffa1c255cf682d86ec4a36
immprada <im.mprada@gmail.com>
2023-04-11 10:13:14 -0500

    feat: add controllers and routes for clubhouses, users, posts, and comments
    Add new controllers for clubhouses, users, posts, and comments with empty
    actions for new, create, show, edit, update, and destroy. Add routes for
    these controllers using the resources method. Set the root path to the
    clubhouses index action.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  root 'clubhouses#index'
+
+  resources :clubhouses
+  resources :users, only: %i[new create show edit update destroy]
+  resources :posts, only: %i[new create show destroy]
+  resources :comments, only: %i[new create edit update destroy]
 end
