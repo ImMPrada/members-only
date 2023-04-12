6066749269e1c7b3cde1de8630f03ab333091a22
immprada <im.mprada@gmail.com>
2023-04-12 15:47:43 -0500

    feat(users): add registrations and sessions controllers for Devise feat(users): add scoped views for Devise feat(users): change root path to show user profile refactor(users): remove unused actions from UsersController feat(users): add avatar and username to user profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,7 @@ class UsersController < ApplicationController
   EMAIL_PLACEHOLDER = 'example@mail.com'.freeze
   PASSWORD_PLACEHOLDER = '********'.freeze
 
-  def new
-    @user = User.new
-  end
-
-  def create; end
+  def index; end
 
   def show; end
-
-  def edit; end
-
-  def update; end
-
-  def destroy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  # devise_scope :user do
+  #   get 'sign_in', to: 'devise/sessions#new'
+  #   get 'sign_in', to: 'devise/sessions#new'
+  #   post 'sign_in', to: 'devise/sessions#create'
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
 
-  root 'users#new'
+  root 'users#show'
 
-  resources :users, only: %i[new create show edit update destroy]
+  # resources :users, only: %i[clubhouses_or_user_profile show]
+  # resources :clubhouses, only: %i[index]
 end
