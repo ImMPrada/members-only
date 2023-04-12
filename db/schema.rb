cd3fd0df400d4b2e36e50a6c5d246098f5ed2410
immprada <im.mprada@gmail.com>
2023-04-12 12:38:21 -0500

    feat(user.rb): add devise modules to User model feat(20230412173558_add_devise_to_users.rb): add devise columns to users table feat(schema.rb): update schema version to reflect devise migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_144721) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_173558) do
   create_table "clubhouses", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
