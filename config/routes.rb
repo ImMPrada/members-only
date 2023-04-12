f2dba5987b2843d659f89d300a1b164aaa375254
immprada <im.mprada@gmail.com>
2023-04-12 18:29:34 -0500

    feat(routes.rb): add new and create actions to clubhouses resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  # devise_scope :user do
-  #   get 'sign_in', to: 'devise/sessions#new'
-  #   get 'sign_in', to: 'devise/sessions#new'
-  #   post 'sign_in', to: 'devise/sessions#create'
-  # end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 
   root 'clubhouses#index'
 
   resources :users, only: %i[show new create]
-  resources :clubhouses, only: %i[index]
+  resources :clubhouses, only: %i[index new create]
 end
