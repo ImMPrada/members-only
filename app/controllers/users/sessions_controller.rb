bca75e31d710cd723d4d496260e555e3ae2dc446
immprada <im.mprada@gmail.com>
2023-04-13 08:32:00 -0500

    feat(sessions_controller.rb): add custom authentication logic to sign in action feat(sessions_controller.rb): add custom sanitizer to permit identificator parameter feat(sessions_controller.rb): add helper methods to find user and handle wrong results fix(sessions_controller.rb): change model from @user to @session in form_with fix(sessions_controller.rb): change url from registration_path to user_session_path feat(new.html.erb): change email field to identificator field feat(devise.rb): change unlock_keys from email to identificator

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,20 +10,46 @@ module Users
     # end
 
     # POST /resource/sign_in
-    # def create
-    #   super
-    # end
+    def create
+      puts 'TEA'
+      user = find_user
+
+      unless user
+        wrong_result and return
+      end
+
+      if user&.valid_password?(params[:password])
+        set_flash_message :notice, :signed_in
+        sign_in(user)
+        redirect_to user_path(user)
+      else
+        wrong_result
+      end
+      # super
+    end
 
     # DELETE /resource/sign_out
     # def destroy
     #   super
     # end
 
-    # protected
+    protected
 
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_in_params
-    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-    # end
+    def configure_sign_in_params
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:identificator])
+    end
+
+    def wrong_result
+      flash[:alert] = 'Wrong user or password.'
+      redirect_to new_user_session_path
+    end
+
+    def find_user
+      identificator = params[:identificator]
+      user_by_email = User.find_by(email: identificator)
+      return user_by_email if user_by_email
+
+      User.find_by(username: identificator)
+    end
   end
 end
