bd179c6da12593412251f8072fc4cf66a43e7010
immprada <im.mprada@gmail.com>
2023-04-14 12:31:53 -0500

    refactor(comments_controller.rb): refactor CommentsController#create method to use instance variables and respond_to block feat(comments_controller.rb): add private method to update post comments list using turbo streams and respond_to block

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,46 @@
 class CommentsController < ApplicationController
-  def new; end
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
 
-  def create; end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = @post
+
+    if @comment.save
+      respond_to { |format| update_post_comments_list(format) }
+    else
+      respond_to do |format|
+        format.html { render :new, notice: 'Post was not created.' }
+      end
+    end
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def update_post_comments_list(format)
+    format.turbo_stream do
+      render turbo_stream: [
+        turbo_stream.prepend(:post_comments_list,
+                             partial: 'partials/comments/card',
+                             locals: { comment: @comment }),
+        turbo_stream.update(:new_post_comment_form, ''),
+        turbo_stream.update(:post_comments_list_empty_message, '')
+      ]
+    end
+    format.html { redirect_to post_path(@post), notice: 'User was successfully updated.' }
+  end
 end
