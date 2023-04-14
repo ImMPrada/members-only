1db64478a2b4e1f050194874bb6bdc8772e1db02
immprada <im.mprada@gmail.com>
2023-04-14 11:13:54 -0500

    feat(posts_controller.rb): add new and create actions to PostsController
    The new action sets up a new Post object and assigns it to an instance variable. The create action creates a new Post object with the permitted parameters and assigns it to an instance variable. It then sets the user and clubhouse associations for the Post object. If the Post object is saved successfully, it updates the clubhouse posts list with a turbo stream and redirects to the clubhouse page. If the Post object is not saved successfully, it renders the new template with a notice.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,46 @@
 class PostsController < ApplicationController
-  def new; end
+  def new
+    @clubhouse = Clubhouse.find(params[:clubhouse_id])
+    @post = Post.new
+  end
 
-  def create; end
+  def create
+    @clubhouse = Clubhouse.find(params[:clubhouse_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.clubhouse = @clubhouse
+
+    if @post.save
+      respond_to { |format| update_clubhouse_posts_list(format) }
+    else
+      respond_to do |format|
+        format.html { render :new, notice: 'Post was not created.' }
+      end
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def update_clubhouse_posts_list(format)
+    format.turbo_stream do
+      render turbo_stream: [
+        turbo_stream.prepend(:clubhouse_posts_list,
+                             partial: 'partials/posts/card',
+                             locals: { post: @post }),
+        turbo_stream.update(:new_post_form, ''),
+        turbo_stream.update(:clubhouse_posts_list_empty_message, '')
+      ]
+    end
+    format.html { redirect_to clubhouse_path(@clubhouse), notice: 'User was successfully updated.' }
+  end
 end
